fe615ebd8863ac7e7c2840f89a7ae3eb58a4d307
Patrick Taylor <ptaylor@dsgonline.com>
2014-12-02 12:42:09 -0500

    Adjusting the project structure.

---
+++ NREPPAdminDB/Tables/Users.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Username] VARCHAR(30) NOT NULL, 
+    [Firstname] VARCHAR(50) NULL, 
+    [Lastname] VARCHAR(50) NULL, 
+    [hash] VARCHAR(100) NOT NULL, 
+    [RoleID] INT NOT NULL, 
+    [salt] VARCHAR(100) NOT NULL, 
+    CONSTRAINT [FK_Users_ToRole] FOREIGN KEY ([RoleID]) REFERENCES [Roles]([Id])
+)
---
+++ NREPPAdminSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NREPPAdminSite.MvcApplication" Language="C#" %>
