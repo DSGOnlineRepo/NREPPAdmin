3d933c03adbc18a40490c1b84ce84d8a2778b103
Patrick Taylor <ptaylor@dsgonline.com>
2014-12-03 10:19:18 -0500

    Changed the nature of the user creation script

---
+++ NREPPAdminDB/Scripts/DBUserSetup.sql
@@ -0,0 +1,16 @@
+﻿/*
+ * Creates database users. You will need to run this manually for the time being.
+*/
+
+CREATE LOGIN nrAdmin WITH PASSWORD = 'nr!Admin123456';
+GO
+
+CREATE LOGIN nrAgent WITH PASSWORD = 'nr!Agent123456';
+GO
+
+CREATE USER [nrAdmin] FOR LOGIN nrAdmin
+EXEC sp_addrolemember N'db_owner', N'nrAdmin'
+GO
+
+CREATE USER [nrAgent] FOR LOGIN nrAgent;
+GRANT EXECUTE ON SCHEMA :: dbo To nrAgent;
