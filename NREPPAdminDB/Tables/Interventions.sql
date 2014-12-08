a71b7567c61ccaf22607fbede58ad59e9b0cca53
Patrick Taylor <ptaylor@dsgonline.com>
2014-12-08 16:05:18 -0500

    Added Interventions Table

---
+++ NREPPAdminDB/Tables/Interventions.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Interventions]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Title] VARCHAR(50) NOT NULL, 
+    [FullDescription] NTEXT NULL, 
+    [Submitter] INT NOT NULL, 
+    [UpdateDate] DATETIME NULL, 
+    [PublishDate] DATE NULL, 
+    CONSTRAINT [FK_Interventions_Users] FOREIGN KEY ([Id]) REFERENCES [Users]([Id]) 
+)
