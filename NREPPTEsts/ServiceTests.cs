6b4ee2a5d8caf497289fa14020f77b412087b8cb
Patrick Taylor <ptaylor@dsgonline.com>
2014-12-11 18:44:48 -0500

    Added test project and initial test

---
+++ NREPPTEsts/ServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NREPPAdminSite;
+using NREPPAdminSite.Models;
+using System.Collections.Generic;
+
+namespace NREPPTests
+{
+    [TestClass]
+    public class ServiceTests
+    {
+        [TestMethod]
+        public void DummyMethod()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestDB()
+        {
+            NrepServ aService = new NrepServ("server=localhost;database=NREPPAdmin;uid=nrAgent;password=nr!Agent123456");
+            List<Intervention> interVs = aService.GetInterventions();
+
+            Assert.IsTrue(interVs.Count > 0);
+            
+        }
+    }
+}
