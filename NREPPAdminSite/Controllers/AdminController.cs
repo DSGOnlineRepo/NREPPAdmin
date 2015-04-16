d37063c216bab80162e40ee735f5cd65412635b3
Patrick Taylor <ptaylor@dsgonline.com>
2015-04-16 15:53:44 -0400

    Committing the changes that add string for Join Program

--- NREPPAdminSite/Controllers/AdminController.cs
+++ NREPPAdminSite/Controllers/AdminController.cs
@@ -122,5 +122,44 @@ namespace NREPPAdminSite.Controllers
             return View(model);
         }
 
+        public ActionResult JoinProgram(string ID)
+        {
+            string nonURLVersion = HttpUtility.UrlDecode(ID).Replace(' ', '+');
+            string decryptedString = PasswordHash.AESDecrypt(nonURLVersion);
+            string[] idBits = decryptedString.Split(';');
+            TempClass aClass = new TempClass();
+
+            // Check the date of the token
+            DateTime TokenDate = DateTime.Parse(idBits[2]);
+            TimeSpan difference = DateTime.Now - TokenDate;
+            if (difference.Days >  5) // This should be made into some kind of app setting/constant
+            {
+                aClass.CanView = false;
+            }
+            else
+            {
+                aClass.CanView = true;
+                aClass.InterventionId = int.Parse(idBits[0]);
+            }
+
+            return View(aClass);
+        }
+
+        public ActionResult GenReviewLink(int Id, int ReviewerId)
+        {
+            ViewBag.SomeString = "somestring";
+
+            string combinedString = string.Format("{0};{1};{2}", Id, ReviewerId, DateTime.Now.ToShortDateString());
+            ViewBag.OutString = HttpUtility.UrlEncode(PasswordHash.AESCrypt(combinedString));
+
+            return View();
+        }
+
+    }
+
+    public class TempClass
+    {
+        public bool CanView { get; set; }
+        public int InterventionId { get; set; }
     }
 }
