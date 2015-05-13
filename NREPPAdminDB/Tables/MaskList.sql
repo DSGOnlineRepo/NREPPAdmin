0b75184fd1f3a2ea4d6934f39335bc8acb4bd22b
Venkata Pratti <srikanth964@gmail.com>
2015-05-13 11:29:49 -0400

    Fixed length of the MaskedValueName

--- NREPPAdminDB/Tables/MaskList.sql
+++ NREPPAdminDB/Tables/MaskList.sql
@@ -2,10 +2,6 @@
 (
 	[Id] INT IDENTITY NOT NULL PRIMARY KEY, 
     [MaskPower] INT NOT NULL, 
-<<<<<<< HEAD
-    [MaskValueName] VARCHAR(350) NULL, 
-=======
     [MaskValueName] VARCHAR(500) NULL, 
->>>>>>> dev
     [MaskCategory] VARCHAR(50) NULL
 )
