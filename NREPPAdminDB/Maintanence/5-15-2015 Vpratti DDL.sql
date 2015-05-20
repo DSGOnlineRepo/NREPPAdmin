e5fd50bfe495a7ba9c81fd5510490b2cfe6cad72
Venkata Pratti <srikanth964@gmail.com>
2015-05-20 09:26:42 -0400

    Synched template changes from Dev

--- NREPPAdminDB/Maintanence/5-15-2015 Vpratti DDL.sql
+++ NREPPAdminDB/Maintanence/5-15-2015 Vpratti DDL.sql
@@ -1,4 +1,6 @@
-﻿
-ALTER TABLE InterventionStatus ADD NotifyUser bit default(0)
+﻿ALTER TABLE InterventionStatus ADD NotifyUser bit default(0)
+GO
 ALTER TABLE InterventionStatus ADD NotifySubmitter bit default(0)
+GO
 ALTER TABLE InterventionStatus ADD NotifyRC bit default(0)
+GO
--- NREPPAdminDB/Maintanence/5-15-2015 Vpratti DML.sql
+++ NREPPAdminDB/Maintanence/5-15-2015 Vpratti DML.sql
@@ -1,1 +1,4 @@
 ﻿INSERT INTO AspNetRoles (Id, Name) VALUES (NEWID(), 'Pre-Screener');
+INSERT INTO AspNetRoles (Id, Name) VALUES (NEWID(), 'Mathematica Review Coordinator');
+INSERT INTO AspNetRoles (Id, Name) VALUES (NEWID(), 'Mathematica PRM');
+UPDATE AspNetRoles SET NAME = 'PRM' WHERE NAME = 'DSG PRM'
