0a21eb9dbc043e8a9241475750a763cb6249f570
Patrick Taylor <ptaylor@dsgonline.com>
2015-05-28 16:21:04 -0400

    Bug fixes for assignment

--- NREPPAdminDB/Maintanence/Patrick_5_28_2015.ddl.sql
+++ NREPPAdminDB/Maintanence/Patrick_5_28_2015.ddl.sql
@@ -1,8 +1,10 @@
-﻿/*
+﻿-- Nothing to See here
+
+/*
 	Make sure to flesh this out as you get more functionality to implement
 */
 
-CREATE PROCEDURE [dbo].[SPChangeInterventionStatus]
+ALTER PROCEDURE [dbo].[SPChangeInterventionStatus]
 	@IntervId int,
 	@User nvarchar(128) = NULL, -- Person performing the operation
 	@DestStatus int,
@@ -104,5 +106,27 @@ AS SET NOCOUNT ON
 			END
 
 			
+	COMMIT TRANSACTION
+RETURN 0
+GO
+
+ALTER PROCEDURE [dbo].[SPAssignUser]
+	@InvId int = 0,
+	@UserId varchar(128),
+	@RoleId varchar(128)
+AS
+	BEGIN TRANSACTION
+
+	IF NOT EXISTS(Select top(1) UserId from Inter_User_Roles WHERE InterventionId = @InvId AND UserId = @UserId AND @RoleId = @RoleId) BEGIN
+	
+	INSERT INTO Inter_User_Roles (InterventionId, UserId, WkRoleId) VALUES (@InvId, @UserId, @RoleId)
+
+	IF @@ERROR <> 0 BEGIN
+		ROLLBACK TRANSACTION
+		RETURN -1
+	END
+
+	END
+
 	COMMIT TRANSACTION
 RETURN 0
