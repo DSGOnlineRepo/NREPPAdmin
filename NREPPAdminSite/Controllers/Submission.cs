1707a3a94b1f901b9481683fe87165dea558b7c3
Patrick Taylor <ptaylor@dsgonline.com>
2015-06-03 14:58:34 -0400

    You need at least one program evaluation to submit.

--- NREPPAdminSite/Controllers/Submission.cs
+++ NREPPAdminSite/Controllers/Submission.cs
@@ -315,6 +315,12 @@ namespace NREPPAdminSite.Controllers
             if (col["Dest"] == "continue")
             {
                 //return RedirectToAction("ConfirmSub", model.TheIntervention.Id);
+                //model.Documents = 
+                List<InterventionDoc> docz = localService.GetDocuments(model.TheIntervention.Id, null, null).ToList();
+                foreach (InterventionDoc doc in docz)
+                {
+                    model.AddDocument(doc);
+                }
                 return View("ConfirmSub", model);
             }
             else return RedirectToAction("Submission6", new { InvId = model.TheIntervention.Id });
@@ -356,28 +362,6 @@ namespace NREPPAdminSite.Controllers
         public ActionResult DocUpload(DocUploadPage model, FormCollection formCollection)
         {
             NrepServ localService = new NrepServ(NrepServ.ConnString);
-
-            /*if (Request != null)
-            {
-                if (Request.Form["docTypeDD"] == "")
-                {
-                    ModelState.AddModelError("docTypeDD", "You must select a document type!");
-                }
-                else
-                {
-                    HttpPostedFileBase file = Request.Files["UploadedFile"];
-                    if ((file != null) && (file.ContentLength > 0) && !string.IsNullOrEmpty(file.FileName))
-                    {
-                        string fileName = file.FileName;
-                        string fileContentType = file.ContentType;
-                        byte[] fileBytes = new byte[file.ContentLength];
-                        file.InputStream.Read(fileBytes, 0, Convert.ToInt32(file.ContentLength));
-
-                        localService.SaveFileToDB(fileBytes, fileName, User.Identity.Name, "NOT IMPLEMENTED!", int.Parse(Request.Form["TheIntervention.Id"]), false,
-                            -1, Request.Form["FileDescription"], int.Parse(Request.Form["docTypeDD"]), Request.Form["FileTitle"]); // TODO: Add UserId to the Cookie. :|
-                    }
-                }
-            }*/
 
              HttpPostedFileBase file = Request.Files["UploadedFile"];
              if ((file != null) && (file.ContentLength > 0) && !string.IsNullOrEmpty(file.FileName))
