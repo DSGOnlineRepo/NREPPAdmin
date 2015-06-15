073a24cdf4525aaa5db5855bc7b5bab4c8c5d7d0
Patrick Taylor <ptaylor@dsgonline.com>
2015-06-15 14:46:34 -0400

    Fixing outcomes not appearing bug.

---
+++ NREPPAdminDB/Maintanence/Patrick_6_15_2015_ddl.sql
@@ -0,0 +1,98 @@
+﻿GO
+
+ALTER PROCEDURE [dbo].[SPAddOrUpdateOutcomeMeasure]
+	@OutcomeId INT = -1,
+	@OutcomeName VARCHAR(30) = NULL,
+	@OutcomeMeasureId INT = -1,
+	@InterventionId INT = 0,
+	@StudyId INT,
+	@OutcomeMeasure VARCHAR(50) = '',
+	@SAMHSAOutcome INT = 0,
+	@GroupFavored BIT = 0,
+	@PopDescription VARCHAR(50) = '',
+	@SAMHSAPop INT = 0,
+	@EffectReport BIT = 0,
+	@Priority INT = 0,
+	@TaxonomyOutcome INT = 0,
+	@RecommendReview BIT = 0,
+	@DocId INT
+AS
+
+	BEGIN TRANSACTION
+
+	-- Add the outcome if it doesn't already exist
+
+	IF @OutcomeId > 0 BEGIN
+		SELECT @OutcomeName = OutcomeName FROM Outcome
+		WHERE Id = @OutcomeId
+	END
+	ELSE BEGIN
+		INSERT INTO Outcome (OutcomeName, InterventionId) VALUES (@OutcomeName, @InterventionId)
+
+		IF @@ERROR <> 0 BEGIN
+			ROLLBACK TRANSACTION
+			RETURN -1
+		END
+
+		SET @OutcomeId = @@IDENTITY
+	END
+
+	-- Add or Update the Outcome Measure
+
+	IF @OutcomeMeasureId > 0 BEGIN
+		UPDATE OutcomeMeasure
+		SET OutcomeId = @OutcomeId,
+			StudyId = @StudyId,
+			OutcomeMeasure = @OutcomeMeasure,
+			--BaselineEquiv = @BaselineEquiv,
+			--SignificantImpact = @SignificantImpact,
+			GroupFavored = @GroupFavored,
+			PopDescription = @PopDescription,
+			SAMHSAPop = @SAMHSAPop,
+			SAMHSAOutcome = @SAMHSAOutcome,
+			RecommendReview = @RecommendReview,
+			TaxonomyOutcome = @TaxonomyOutcome,
+			EffectReport = @EffectReport,
+			DocId = @DocId
+		WHERE Id = @OutcomeMeasureId
+
+		IF @@ERROR <> 0 BEGIN
+			ROLLBACK TRANSACTION
+			RETURN -2
+		END
+	END
+	ELSE BEGIN
+
+		INSERT INTO OutcomeMeasure (
+			OutcomeId,
+			StudyId,
+			OutcomeMeasure,
+			GroupFavored,
+			PopDescription,
+			SAMHSAPop,
+			RecommendReview,
+			TaxonomyOutcome,
+			SAMHSAOutcome,
+			EffectReport,
+			DocId)
+			VALUES (@OutcomeId,
+			@StudyId,
+			@OutcomeMeasure,
+			@GroupFavored,
+			@PopDescription,
+			@SAMHSAPop,
+			@RecommendReview,
+			@TaxonomyOutcome,
+			@SAMHSAOutcome,
+			@EffectReport,
+			@DocId)
+
+		IF @@ERROR <> 0 BEGIN
+			ROLLBACK TRANSACTION
+			RETURN -3
+		END
+	END
+
+	COMMIT TRANSACTION
+
+RETURN 0
