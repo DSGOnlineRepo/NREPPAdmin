2c470c7695486859d12c0de580015fb45dc9f1bd
Patrick Taylor <ptaylor@dsgonline.com>
2015-06-19 11:06:44 -0400

    Fix for PI role permissions

---
+++ NREPPAdminDB/Maintanence/Patrick_6_19_2015.dml.sql
@@ -0,0 +1,7 @@
+﻿GO
+
+delete from Role_Permissions
+WHERE PermissionId in (select Id from Permissions where PermissionName = 'EditBaseSubmission') AND
+RoleID in (SELECT Id from AspNetRoles WHERE Name = 'Principal Investigator')
+
+GO
