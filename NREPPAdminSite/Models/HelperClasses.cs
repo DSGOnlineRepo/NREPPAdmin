afe214e54a4b2a7e2bcc276d90c688f238a300ca
Patrick Taylor <ptaylor@dsgonline.com>
2015-06-30 11:04:03 -0400

    Text boxes for write-in answers

---
+++ NREPPAdminSite/Models/HelperClasses.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using NREPPAdminSite.Models;
+using System.Linq;
+
+namespace NREPPAdminSite {
+
+    public static class NREPPHtml
+    {
+        public static MvcHtmlString QoRColHeader(RigorQuestion q)
+        {
+            var thTag = new TagBuilder("th");
+            thTag.InnerHtml = q.QuestionString;
+
+            return MvcHtmlString.Create(thTag.ToString());
+        }
+
+        public static MvcHtmlString QoRColHeader(RigorQuestion q, IDictionary<string, object> attribs)
+        {
+            var thTag = new TagBuilder("th");
+            thTag.InnerHtml = q.QuestionString;
+
+            foreach (string k in attribs.Keys)
+                thTag.MergeAttribute(k, attribs[k].ToString());
+
+            return MvcHtmlString.Create(thTag.ToString());
+        }
+
+        public static MvcHtmlString GroupHeader(IEnumerable<RigorQuestion> questions, string HeaderName)
+        {
+            var thTag = new TagBuilder("th");
+
+            IEnumerable<RigorQuestion> someQuestions = questions.Where(x => x.QuestionGroup == HeaderName);
+
+            thTag.MergeAttribute("colspan", someQuestions.Count().ToString());
+            thTag.MergeAttribute("style", "text-align: center"); // Remove this before sending to Lisa
+            thTag.InnerHtml = HeaderName;
+
+            return MvcHtmlString.Create(thTag.ToString());
+        }
+    }
+
+}
